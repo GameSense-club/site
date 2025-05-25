87ece08667443533032a44880f4473823f573efe
falbue <cyansair05@gmail.com>
2025-05-25 17:49:36 +0500

    Мини улучшения

--- app.py
+++ app.py
@@ -6,10 +6,6 @@ app = Flask(__name__)
 def hello_world():
     return render_template("index.html")
 
-@app.route('/favicon.ico')
-def favicon():
-    return send_from_directory(app.static_folder, 'lib/ghonse.svg', mimetype='image/svg+xml')
-
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
