959eaf17012bcbfde7e5b158fa5f7374d3d7b1cf
falbue <cyansair05@gmail.com>
2025-05-25 21:34:51 +0500

    Создана основа

--- app.py
+++ app.py
@@ -3,8 +3,18 @@ from flask import Flask, render_template, send_from_directory
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return render_template("index.html")
+def index():
+    return render_template("shop.html")
+
+@app.route('/profile')
+def profile():
+    return render_template("profile.html")
+
+@app.route('/computers')
+def computers():
+    return render_template("computers.html")
+
+
 
 
 if __name__ == '__main__':
