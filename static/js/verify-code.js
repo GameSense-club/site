09c8d8bbb72d2ec8c7b059fa52bc5909c2515de6
falbue <cyansair05@gmail.com>
2025-05-30 01:23:51 +0500

    Теперь код отправляется при заходе на страницу

--- static/js/verify-code.js
+++ static/js/verify-code.js
@@ -1,70 +1,82 @@
-		function getCookie(name) {
-			const value = `; ${document.cookie}`;
-			const parts = value.split(`; ${name}=`);
-			if (parts.length === 2) return parts.pop().split(';').shift();
-		}
+const userData = localStorage.getItem('user');
+const user = JSON.parse(userData);
+const email = user?.email || 'Email не найден'; // Защита от undefined
 
-		const userData = localStorage.getItem('user');
-		const user = JSON.parse(userData);
-    const email = user?.email || 'Email не найден'; // Защита от undefined
-    document.getElementById("email").textContent = email;
+fetch('https://api.game-sense.net/verify-code/send', {
+		method: 'POST',
+		headers: {
+			'Authorization': `Bearer ${jwtToken}`,
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify({email:email})
+	})
 
-    const inputs = document.querySelectorAll('input');
-    const resendBtn = document.getElementById('resendBtn');
+
+function getCookie(name) {
+	const value = `; ${document.cookie}`;
+	const parts = value.split(`; ${name}=`);
+	if (parts.length === 2) return parts.pop().split(';').shift();
+}
+
+
+document.getElementById("email").textContent = email;
+
+const inputs = document.querySelectorAll('input');
+const resendBtn = document.getElementById('resendBtn');
 
     // Фокус на первое поле
-    inputs[0].focus();
+inputs[0].focus();
 
     // Переключение фокуса при вводе
-    inputs.forEach((input, index) => {
-    	input.addEventListener('input', () => {
-    		if (input.value.length === 1 && index < inputs.length - 1) {
-    			inputs[index + 1].focus();
-    		}
+inputs.forEach((input, index) => {
+	input.addEventListener('input', () => {
+		if (input.value.length === 1 && index < inputs.length - 1) {
+			inputs[index + 1].focus();
+		}
 
             // Проверка, заполнены ли все поля
-    		const allFilled = Array.from(inputs).every(input => input.value.length === 1);
-    		if (allFilled) {
+		const allFilled = Array.from(inputs).every(input => input.value.length === 1);
+		if (allFilled) {
                 sendData(); // Отправляем данные, когда все поля заполнены
             }
         });
 
         // Поддержка Backspace для перемещения назад
-    	input.addEventListener('keydown', (e) => {
-    		if (e.key === 'Backspace' && input.value === '' && index > 0) {
-    			inputs[index - 1].focus();
-    		}
-    	});
-    });
+	input.addEventListener('keydown', (e) => {
+		if (e.key === 'Backspace' && input.value === '' && index > 0) {
+			inputs[index - 1].focus();
+		}
+	});
+});
 
 function sendData() {
-    const code = Array.from(inputs).map(input => input.value).join('');
-    const jwtToken = getCookie('jwt_token');
-    const data = { email: email, code: code };
+	const code = Array.from(inputs).map(input => input.value).join('');
+	const jwtToken = getCookie('jwt_token');
+	const data = { email: email, code: code };
 
-    fetch('https://api.game-sense.net/verify-code ', {
-        method: 'POST',
-        headers: {
-            'Authorization': `Bearer ${jwtToken}`,
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    })
-    .then(response => {
-        if (!response.ok) {
+	fetch('https://api.game-sense.net/verify-code ', {
+		method: 'POST',
+		headers: {
+			'Authorization': `Bearer ${jwtToken}`,
+			'Content-Type': 'application/json'
+		},
+		body: JSON.stringify(data)
+	})
+	.then(response => {
+		if (!response.ok) {
             // Пробуем получить текст ошибки
-            return response.json()
-                .then(errorData => {
-                    const errorMessage = errorData.error || 'Неверный код';
-                    throw new Error(errorMessage);
-                });
-        }
-        return response.json();
-    })
-    .then(result => {
-        window.location.href = '/';
-    })
-    .catch(error => {
+			return response.json()
+			.then(errorData => {
+				const errorMessage = errorData.error || 'Неверный код';
+				throw new Error(errorMessage);
+			});
+		}
+		return response.json();
+	})
+	.then(result => {
+		window.location.href = '/';
+	})
+	.catch(error => {
         showNotification(error.message); // Показываем конкретную ошибку
     });
 }
