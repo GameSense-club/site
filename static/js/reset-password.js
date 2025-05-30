6cfd7f529452892d6be16118e61a5b3f464cf07c
falbue <cyansair05@gmail.com>
2025-05-31 03:06:33 +0500

    Восстановление пароля

---
+++ static/js/reset-password.js
@@ -0,0 +1,36 @@
+function reset(e) {
+    e.preventDefault();
+    const form = e.target;
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData);
+
+    loading(form, true);
+    
+    fetch('https://api.game-sense.net/reset-password', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Ошибка при получении данных пользователя');
+        }
+        return response.json();
+    })
+    .then(result => {
+        showNotification("Письмо отправлено");
+    })
+    .catch(error => {
+        showNotification(error);
+    })
+    .finally(() => {
+        loading(form, false);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const formReset = document.getElementById('reset-password');
+    formReset.addEventListener('submit', reset);
+});
