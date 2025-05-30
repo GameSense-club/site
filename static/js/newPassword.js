10baab50b8df2782989be5092c5012286b5116e5
falbue <cyansair05@gmail.com>
2025-05-31 03:40:19 +0500

    Для восстановления пароля

---
+++ static/js/newPassword.js
@@ -0,0 +1,38 @@
+function newPassword(e) {
+    e.preventDefault();
+    const form = e.target;
+    
+    const token = window.location.pathname.split('/reset-password/')[1];
+    const password = document.querySelector('input[name="password"]').value;
+
+    loading(form, true);
+    
+    fetch('https://api.game-sense.net/new-password', {
+        method: 'POST',
+        headers: {
+            'Authorization': `Bearer ${token}`,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ password: password }) 
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Ошибка при получении данных пользователя');
+        }
+        return response.json();
+    })
+    .then(result => {
+        window.location.href = "/login";
+    })
+    .catch(error => {
+        showNotification(error);
+    })
+    .finally(() => {
+        loading(form, false);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('newPassword');
+    form.addEventListener('submit', newPassword);
+});
