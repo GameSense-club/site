63f11722512678011303bbdceb9590dd033cde96
falbue <cyansair05@gmail.com>
2025-12-25 22:59:42 +0500

    Новогоднее обновление

--- app.py
+++ app.py
@@ -1,67 +1,74 @@
-from flask import Flask, render_template, send_from_directory, request, redirect, url_for, make_response
+from flask import (
+    Flask,
+    render_template,
+    send_from_directory,
+    request,
+    redirect,
+    url_for,
+    make_response,
+)
 
 app = Flask(__name__)
 
-PUBLIC_ROUTES = {
-    'login',
-    'static',
-    'login_pc',
-    'reset_password',
-    'price'
-}
+PUBLIC_ROUTES = {"login", "static", "login_pc", "reset_password", "price"}
+
 
 @app.before_request
 def require_login():
     if request.endpoint in PUBLIC_ROUTES:
-        return 
+        return
 
-    token = request.cookies.get('jwt_token')
+    token = request.cookies.get("jwt_token")
     if not token:
-        return redirect(url_for('login'))
+        return redirect(url_for("login"))
 
-@app.route('/')
+
+@app.route("/")
 def index():
     return render_template("shop.html")
 
 
-@app.route('/profile')
+@app.route("/profile")
 def profile():
     return render_template("profile.html")
 
-@app.route('/login')
+
+@app.route("/login")
 def login():
     return render_template("login.html")
 
-@app.route('/verify')
+
+@app.route("/verify")
 def verify():
     return render_template("verify.html")
 
-@app.route('/login_pc/<pc_token>')
+
+@app.route("/login_pc/<pc_token>")
 def login_pc(pc_token):
-    response = make_response(redirect(url_for('index')))
-    response.set_cookie(
-        'pc_token',
-        pc_token,
-        path='/'
-    )
+    response = make_response(redirect(url_for("index")))
+    response.set_cookie("pc_token", pc_token, path="/")
     return response
 
-@app.route('/admin')
+
+@app.route("/admin")
 def admin():
     return render_template(f"admin.html")
 
-@app.route('/admin/<action>')
+
+@app.route("/admin/<action>")
 def action_admin(action):
     return render_template(f"{action}.html")
 
-@app.route('/reset-password/<token>')
+
+@app.route("/reset-password/<token>")
 def reset_password(token):
     return render_template("reset.html")
 
-@app.route('/price')
+
+@app.route("/price")
 def price():
     return render_template("price.html")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5001)
